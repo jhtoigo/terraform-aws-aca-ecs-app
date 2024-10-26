186a497baa401cf668dcff1a3e14406691f12d5d
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-10-26 12:25:59 -0300

    feat: day 3 setup do modulo do service

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "region" {}
+
+variable "service_name" {}
+
+variable "cluster_name" {}
+
+variable "ssm_vpc_id" {}
+
+variable "service_port" {}
+
+variable "service_memory" {}
+
+variable "service_cpu" {}
+
+variable "ssm_listener" {}
+
+variable "ssm_private_subnet_1" {}
+
+variable "ssm_private_subnet_2" {}
+
+variable "ssm_private_subnet_3" {}
