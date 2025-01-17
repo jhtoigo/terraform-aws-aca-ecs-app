93b1408ba49a6430661379963e5760f4481e0028
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2025-01-17 10:42:41 -0300

    ci: multiple environments

--- pipeline.sh
+++ pipeline.sh
@@ -33,7 +33,7 @@ cd ../terraform
 echo "TERRAFORM - FORMAT CHECK"
 terraform fmt --recursive --check
 
-terraform init -backend-config=environment/dev/backend.tfvars
+terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
 
 echo "TERRAFORM - VALIDATE"
 terraform validate
@@ -91,3 +91,25 @@ docker tag app:latest $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_N
 echo "BUILD - DOCKER PUBLISH"
 
 docker push $AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
+
+
+
+# APPLY DO TERRAFORM - CD
+
+cd ../terraform
+
+REPOSITORY_TAG=$AWS_ACCOUNT.dkr.ecr.us-east-1.amazonaws.com/$REPOSITORY_NAME:$GIT_COMMIT_HASH
+
+echo "DEPLOY - TERRAFORM INIT"
+terraform init -backend-config=environment/$BRANCH_NAME/backend.tfvars
+
+
+echo "DEPLOY - TERRAFORM PLAN"
+terraform plan -var-file=environment/$BRANCH_NAME/terraform.tfvars -var container_image=$REPOSITORY_TAG
+
+echo "DEPLOY - TERRAFORM APPLY"
+terraform apply --auto-approve -var-file=environment/$BRANCH_NAME/terraform.tfvars -var container_image=$REPOSITORY_TAG
+
+echo "DEPLOY - WAIT DEPLOY"
+
+aws ecs wait services-stable --region $REGION --cluster $CLUSTER_NAME --services $APP_NAME 
